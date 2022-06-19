d934daaaa206ad17ebb05b41d098403e1fe8cdeb
Gusar82 <rusgusa82@gmail.com>
2022-06-19 19:44:24 +0500

    task #1 is done

--- main.py
+++ main.py
@@ -9,6 +9,7 @@
 from requests import get
 import bs4
 from header import HEADERS
+from pprint import pprint
 
 
 def get_soup(url, header):
@@ -18,15 +19,62 @@ def get_soup(url, header):
 
 
 def get_articles_previw(soup):
-    pass
+    class_ = 'tm-article-body tm-article-snippet__lead'
+    # class_v2 = "article-formatted-body article-formatted-body article-formatted-body_version-2"
+    # class_v1 = "article-formatted-body article-formatted-body article-formatted-body_version-1"
+    previw = soup.find(class_=class_)
+    # if previw:
+        # print(previw.prettify())
+    # print(previw.get_text())
+    return previw.get_text()
 
 
-def search_keywords(article):
-    pass
+def search_keywords(text, keywords):
+    # for keyword in keywords:
+    #     if text.lower().find(keyword.lower()) != -1:
+    #         return True
+    # return False
+    set_search = set(text.lower().split()) & set(keywords)
+    return bool(set_search)
 
 
 def get_date_articles(soup):
+    time = soup.find('time')
+    # print(time.attrs)
+    return time['title']
+
+
+def get_title_articles(soup):
+    title = soup.find(class_="tm-article-snippet__title-link")
+    # print(title.prettify())
+    return title.span.text
+
+
+def get_href_articles(soup, base_url):
+    title = soup.find(class_="tm-article-snippet__title-link")
+    return base_url+title['href']
+
+
+def output_article(soup, base_url):
+    """в формате: <дата> - <заголовок> - <ссылка>"""
+    date = get_date_articles(soup)
+    title = get_title_articles(soup)
+    href = get_href_articles(soup, base_url)
+    return f"<{date}> - <{title}> - <{href}>"
     pass
+
+
+def main():
+    KEYWORDS = ['дизайн', 'фото', 'web', 'python', 'интеллект']
+    base_url = 'https://habr.com'
+    url = base_url + '/ru/all/'
+    soup = get_soup(url, HEADERS)
+    articles = soup.find_all('article')
+    for article in articles:
+        # pprint(article.prettify())
+        text_article = get_articles_previw(article)
+        if search_keywords(text_article, KEYWORDS):
+            print(output_article(article, base_url))
 
 
 def get_articles_all_text(soup):
@@ -34,7 +82,4 @@ def get_articles_all_text(soup):
 
 
 if __name__ == '__main__':
-    KEYWORDS = ['дизайн', 'фото', 'web', 'python']
-    base_url = 'https://habr.com'
-    url = base_url + '/ru/all/'
-    soup = get_soup(base_url, HEADERS)
+    main()
